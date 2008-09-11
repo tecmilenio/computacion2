7e8863d05c032003f06c8aa78a93dcf21e88b310
Sebastian Aviña <sebastianavina@gmail.com>
2008-09-11 15:15:59 -0500

    Se agrego la primera linea para que el interprete lo encuentre

--- sesion02/comandos_linux_sesion2.sh
+++ sesion02/comandos_linux_sesion2.sh
@@ -1,53 +1,55 @@
-    1  ifconfig
-    2  ping 
-    3  ping 207.44.186.70
-    4  sudo apt-get install emacs
-    5  sudo aptitude update
-    6  sudo apt-get install build-essential
-    7  sudo apt-get install emacs
-    8  g++
-    9  cd /home/tecmilenio/Document
-   10  cd /home/tecmilenio/Documentos/
-   11  ls
-   12  mkdir compu2
-   13  ls
-   14  cd compu2
-   15  mkdir sesion2
-   16  cd /home/tecmilenio/Escritorio/
-   17  g++ primeravance.cc primeravance
-   18  g++ primeravance.cc -o primeravance
-   19  ./primeravance 
-   20  sudo apt-get install git
-   21  CD ~/.SSH
-   22  CD ~/.ssh
-   23  mkdir .ssh
-   24  cd ~/.ssh
-   25  ssh-keygen -t rsa
-   26  cat id_rsa
-   27  cat id_rsa.pub 
-   28  cd ~/
-   29  ls
-   30  cd Documentos/
-   31  ls
-   32  cd compu2/
-   33  ls
-   34  git init
-   35  touch README
-   36  emacs -nw README 
-   37  cat README
-   38  git add -
-   39  git add .
-   40  sudo apt-get install git-core
-   41  git add .
-   42  git init
-   43  git add .
-   44  git commit -m "Nuestra primera actualizacion"
-   45  git remote add origin git@github.com:tecmilenio/computacion2.git
-   46  git push origin master
-   47  cd sesion2/
-   48  ls
-   49  emacs -nw integral
-   50  git add .
-   51  git commit -m "Esta es la segunda tarea"
-   52  git push origin master
-   53  history > comandos_linux_sesion2.txt
+"!/bin/bash
+
+ifconfig
+ping 
+ping 207.44.186.70
+sudo apt-get install emacs
+sudo aptitude update
+sudo apt-get install build-essential
+sudo apt-get install emacs
+g++
+cd /home/tecmilenio/Document
+cd /home/tecmilenio/Documentos/
+ls
+mkdir compu2
+ls
+cd compu2
+mkdir sesion2
+cd /home/tecmilenio/Escritorio/
+g++ primeravance.cc primeravance
+g++ primeravance.cc -o primeravance
+./primeravance 
+sudo apt-get install git
+CD ~/.SSH
+CD ~/.ssh
+mkdir .ssh
+cd ~/.ssh
+ssh-keygen -t rsa
+cat id_rsa
+cat id_rsa.pub 
+cd ~/
+ls
+cd Documentos/
+ls
+cd compu2/
+ls
+git init
+touch README
+emacs -nw README 
+cat README
+git add -
+git add .
+sudo apt-get install git-core
+git add .
+git init
+git add .
+git commit -m "Nuestra primera actualizacion"
+git remote add origin git@github.com:tecmilenio/computacion2.git
+git push origin master
+cd sesion2/
+ls
+emacs -nw integral
+git add .
+git commit -m "Esta es la segunda tarea"
+git push origin master
+history > comandos_linux_sesion2.txt
